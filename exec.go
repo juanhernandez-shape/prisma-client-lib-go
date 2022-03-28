e5a9c55f33cffc47529ba24c875af5bca927581b
Juan Hernande <juan@shape.dk>
2022-03-28 13:39:58 +0200

    Adding fix

--- exec.go
+++ exec.go
@@ -3,7 +3,7 @@ package prisma
 import (
 	"context"
 	"strconv"
-
+    "fmt"
 	"github.com/mitchellh/mapstructure"
 )
 
@@ -47,7 +47,7 @@ func (exec *Exec) buildQuery() (string, map[string]interface{}) {
 				}
 			}
 			allArgs = append(allArgs, *arg)
-			variables[arg.Name] = arg.Value
+			variables[fmt.Sprintf("%s_%s", instruction.Name, arg.Name)] = arg.Value
 		}
 	}
 	query := exec.Client.ProcessInstructions(exec.Stack)
